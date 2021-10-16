f90fe5af91b044f4141e4b41b1fa374520580ff7
turtton <watagame2579@gmail.com>
2021-10-16 19:16:27 +0900

    :hammer: exchange Array classes

--- src/commonMain/kotlin/ItemModelStructure.kt
+++ src/commonMain/kotlin/ItemModelStructure.kt
@@ -3,137 +3,50 @@ package net.lepinoid.bbdatastructure
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.lepinoid.bbdatastructure.serializer.ItemCubeSerializer
-import net.lepinoid.bbdatastructure.util.Axis
-import net.lepinoid.bbdatastructure.util.Direction
-import net.lepinoid.bbdatastructure.util.ItemOutLiner
+import net.lepinoid.bbdatastructure.serializer.vector.ArrayLikeVectorSerializer
+import net.lepinoid.bbdatastructure.util.*
 
+/**
+ * ItemModelに対応するDataClass
+ * 一部Blockbenchからのみ出力される専用のデータにも対応しています
+ */
 @Serializable
 data class ItemModelData(
     var credit: String? = null,
     @SerialName("texture_size")
-    var textureSize: IntArray? = null,
+    var textureSize: Plane? = null,
     var textures: Map<String, String>,
     @SerialName("gui_light")
     var guiLight: String? = null,
     var elements: List<ItemElement>,
     var display: ItemDisplay? = null,
     var groups: List<ItemOutLiner>? = null
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemModelData
-
-        if (credit != other.credit) return false
-        if (textureSize != null) {
-            if (other.textureSize == null) return false
-            if (!textureSize.contentEquals(other.textureSize)) return false
-        } else if (other.textureSize != null) return false
-        if (textures != other.textures) return false
-        if (guiLight != other.guiLight) return false
-        if (elements != other.elements) return false
-        if (display != other.display) return false
-        if (groups != other.groups) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = credit?.hashCode() ?: 0
-        result = 31 * result + (textureSize?.contentHashCode() ?: 0)
-        result = 31 * result + textures.hashCode()
-        result = 31 * result + (guiLight?.hashCode() ?: 0)
-        result = 31 * result + elements.hashCode()
-        result = 31 * result + (display?.hashCode() ?: 0)
-        result = 31 * result + (groups?.hashCode() ?: 0)
-        return result
-    }
-}
+)
 
 @Serializable
 data class ItemElement(
-    var from: DoubleArray,
-    var to: DoubleArray,
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var from: Vector,
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var to: Vector,
     var rotation: ItemRotation? = null,
     var faces: Map<Direction, ItemElementFace>
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemElement
-
-        if (!from.contentEquals(other.from)) return false
-        if (!to.contentEquals(other.to)) return false
-        if (rotation != other.rotation) return false
-        if (faces != other.faces) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = from.contentHashCode()
-        result = 31 * result + to.contentHashCode()
-        result = 31 * result + (rotation?.hashCode() ?: 0)
-        result = 31 * result + faces.hashCode()
-        return result
-    }
-}
+)
 
 @Serializable
 data class ItemRotation(
     var angle: Double,
     var axis: Axis,
-    var origin: DoubleArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemRotation
-
-        if (angle != other.angle) return false
-        if (axis != other.axis) return false
-        if (!origin.contentEquals(other.origin)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = angle.hashCode()
-        result = 31 * result + axis.hashCode()
-        result = 31 * result + origin.contentHashCode()
-        return result
-    }
-}
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var origin: Vector
+)
 
 @Serializable
 data class ItemElementFace(
-    var uv: DoubleArray,
+    var uv: UV,
     var rotation: Double? = null,
     var texture: String
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemElementFace
-
-        if (!uv.contentEquals(other.uv)) return false
-        if (rotation != other.rotation) return false
-        if (texture != other.texture) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = uv.contentHashCode()
-        result = 31 * result + (rotation?.hashCode() ?: 0)
-        result = 31 * result + texture.hashCode()
-        return result
-    }
-}
+)
 
 @Serializable
 data class ItemDisplay(
@@ -145,57 +58,21 @@ data class ItemDisplay(
 
 @Serializable
 data class ItemDisplayData(
-    var rotation: DoubleArray,
-    var translation: DoubleArray,
-    var scale: DoubleArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemDisplayData
-
-        if (!rotation.contentEquals(other.rotation)) return false
-        if (!translation.contentEquals(other.translation)) return false
-        if (!scale.contentEquals(other.scale)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = rotation.contentHashCode()
-        result = 31 * result + translation.contentHashCode()
-        result = 31 * result + scale.contentHashCode()
-        return result
-    }
-}
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var rotation: Vector,
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var translation: Vector,
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var scale: Vector
+)
 
 @Serializable
 data class ItemGroup(
     var name: String,
-    var origin: DoubleArray,
-    var children: IntArray
-) : ItemOutLiner {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ItemGroup
-
-        if (name != other.name) return false
-        if (!origin.contentEquals(other.origin)) return false
-        if (!children.contentEquals(other.children)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + origin.contentHashCode()
-        result = 31 * result + children.contentHashCode()
-        return result
-    }
-}
+    @Serializable(with = ArrayLikeVectorSerializer::class)
+    var origin: Vector,
+    var children: Plane
+) : ItemOutLiner
 
 @Serializable(with = ItemCubeSerializer::class)
 data class ItemCube(val id: Int) : ItemOutLiner

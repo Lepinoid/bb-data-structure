58610c216c5d07efdadd773b6110b02e8b55976c
turtton(watagame) <39013345+turtton@users.noreply.github.com>
2022-04-04 21:12:56 +0900

    fix Vector class (#21)
    * :hammer: make fields immutable
    
    * :hammer: add some functional methods and values
    
    * :hammer: update Vector test

--- src/commonMain/kotlin/util/Vector.kt
+++ src/commonMain/kotlin/util/Vector.kt
@@ -5,7 +5,7 @@ import net.lepinoid.bbdatastructure.serializer.vector.VectorSerializer
 import kotlin.math.sqrt
 
 @Serializable(with = VectorSerializer::class)
-data class Vector(var x: Double, var y: Double, var z: Double) {
+data class Vector(val x: Double, val y: Double, val z: Double) {
     operator fun plus(other: Vector): Vector = Vector(x + other.x, y + other.y, z + other.z)
 
     operator fun minus(other: Vector): Vector = Vector(x - other.x, y - other.y, z - other.z)
@@ -14,10 +14,22 @@ data class Vector(var x: Double, var y: Double, var z: Double) {
 
     operator fun div(other: Vector): Vector = Vector(x / other.x, y / other.y, z / other.z)
 
+    operator fun plus(value: Double): Vector = Vector(x + value, y + value, z + value)
+
+    operator fun minus(value: Double): Vector = Vector(x - value, y - value, z - value)
+
+    operator fun times(value: Double): Vector = Vector(x * value, y * value, z * value)
+
+    operator fun div(value: Double): Vector = Vector(x / value, y / value, z / value)
+
     val lengthSquared: Double
         get() = x * x + y * y + z * z
 
     @Suppress("UNUSED")
     val length: Double
         get() = sqrt(lengthSquared)
+
+    companion object {
+        val ZERO = Vector(0.0, 0.0, 0.0)
+    }
 }

39c03849bbb9f10947d3e828b86d9ce9edb6619a
turtton <top.gear7509@turtton.net>
2022-11-16 22:21:27 +0900

    :hammer: reimplement BBElementSerializer

--- src/commonMain/kotlin/serializer/BBElementSerializer.kt
+++ src/commonMain/kotlin/serializer/BBElementSerializer.kt
@@ -1,23 +1,78 @@
+@file:UseSerializers(UuidSerializer::class)
+
 package net.lepinoid.bbdatastructure.serializer
 
-import kotlinx.serialization.DeserializationStrategy
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import com.benasher44.uuid.Uuid
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.serialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import net.lepinoid.bbdatastructure.BBElementCube
+import net.lepinoid.bbdatastructure.BBElementFace
 import net.lepinoid.bbdatastructure.BBElementLocator
 import net.lepinoid.bbdatastructure.util.BBElement
+import net.lepinoid.bbdatastructure.util.Direction
+import net.lepinoid.bbdatastructure.util.Plane
+import net.lepinoid.bbdatastructure.util.Vector
+import net.lepinoid.uuidserializer.UuidSerializer
 
-object BBElementSerializer : JsonContentPolymorphicSerializer<BBElement>(BBElement::class) {
-    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<out BBElement> {
-        val type = element.jsonObject["type"] ?: return BBElementCube.serializer()
-        if (type.jsonPrimitive.isString) {
-            val typeString = type.jsonPrimitive.content
-            if (typeString == "locator") {
-                return BBElementLocator.serializer()
+object BBElementSerializer : KSerializer<BBElement> {
+    override val descriptor: SerialDescriptor = serialDescriptor<BBElement>()
+
+    override fun deserialize(decoder: Decoder): BBElement {
+        val rawData = BBElementSurrogate.serializer().deserialize(decoder)
+        when (rawData.type) {
+            "cube" -> rawData.run {
+                BBElementCube(
+                    name,
+                    rescale!!,
+                    from,
+                    to!!,
+                    autoUv!!,
+                    color!!,
+                    isLocked,
+                    rotation!!,
+                    origin!!,
+                    uvOffset!!,
+                    faces!!,
+                    uuid,
+                    type
+                )
+            }
+
+            "locator" -> rawData.run {
+                BBElementLocator(name, from, isLocked, uuid, type)
             }
         }
-        error("Cannot serialize elements. [type] shows incompatible data: $type")
+        error("Cannot serialize elements. [type] shows incompatible data: ${rawData.type}")
     }
+
+    override fun serialize(encoder: Encoder, value: BBElement) {
+        when (value) {
+            is BBElementCube -> BBElementCube.serializer().serialize(encoder, value)
+            is BBElementLocator -> BBElementLocator.serializer().serialize(encoder, value)
+        }
+    }
+
+    @Serializable
+    private class BBElementSurrogate(
+        val name: String,
+        val from: Vector,
+        @SerialName("locked")
+        val isLocked: Boolean,
+        val uuid: Uuid,
+        val type: String = "cube",
+        // Cube
+        val rescale: Boolean? = null,
+        val to: Vector? = null,
+        @SerialName("autouv")
+        val autoUv: Int? = null,
+        val color: Long? = null,
+        val rotation: Vector? = null,
+        val origin: Vector? = null,
+        @SerialName("uv_offset")
+        val uvOffset: Plane? = null,
+        val faces: Map<Direction, BBElementFace>? = null
+    )
 }
